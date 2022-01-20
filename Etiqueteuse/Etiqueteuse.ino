e507d5e39aea727adb5d0245b9ad053f1bc838ce
emmameunier <emma.meunier166@gmail.com>
2022-01-20 12:59:07 +0100

    remise au propre

--- Etiqueteuse/Etiqueteuse.ino
+++ Etiqueteuse/Etiqueteuse.ino
@@ -1,17 +1,32 @@
+#include <Arduino.h>
+#include <NewPing.h>
 
-//** Name : 
-//** Programmer : 
-//** Version :
+#define trigPin 2
+#define echoPin 3
+#define cmdRelay 10
+#define DISTANCE_DETECT 5
+#define DISTANCE_MAX 20
+#define TIME_DELAY 2000
+bool bouteilleTrouvee = false;
+NewPing sonar = NewPing(trigPin, echoPin,DISTANCE_MAX);
 
-//** Globals
-int countBouteilles;
-
-void setup() {
-  // put your setup code here, to run once:
-
+void setup(){
+  Serial.begin(9600);
+  pinMode(cmdRelay, OUTPUT);
+  digitalWrite(cmdRelay, HIGH);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop(){
+  float distance = sonar.ping_cm();
+  if (0<distance && distance < DISTANCE_DETECT && bouteilleTrouvee == false ){
+    bouteilleTrouvee = true;
+    digitalWrite(cmdRelay, LOW);
+    delay(TIME_DELAY);
+    digitalWrite(cmdRelay, HIGH);
+  }
+  else if(bouteilleTrouvee== true && (distance == 0 || distance >DISTANCE_DETECT)){
+    bouteilleTrouvee = false;
+    Serial.println("bouteille trouvée");
+  }
 }
