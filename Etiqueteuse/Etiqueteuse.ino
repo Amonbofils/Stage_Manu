78e4278faa9bfd16afb7627f9c159a9d6779aefc
emmameunier <emma.meunier166@gmail.com>
2022-01-20 13:00:58 +0100

    Mise aux normes nom de variables

--- Etiqueteuse/Etiqueteuse.ino
+++ Etiqueteuse/Etiqueteuse.ino
@@ -1,19 +1,19 @@
 #include <Arduino.h>
 #include <NewPing.h>
 
-#define trigPin 2
-#define echoPin 3
-#define cmdRelay 10
+#define TRIG_PIN 2
+#define ECHO_PIN 3
+#define CMD_RELAY 10
 #define DISTANCE_DETECT 5
 #define DISTANCE_MAX 20
 #define TIME_DELAY 2000
 bool bouteilleTrouvee = false;
-NewPing sonar = NewPing(trigPin, echoPin,DISTANCE_MAX);
+NewPing sonar = NewPing(TRIG_PIN, ECHO_PIN,DISTANCE_MAX);
 
 void setup(){
   Serial.begin(9600);
-  pinMode(cmdRelay, OUTPUT);
-  digitalWrite(cmdRelay, HIGH);
+  pinMode(CMD_RELAY, OUTPUT);
+  digitalWrite(CMD_RELAY, HIGH);
 }
 
 
@@ -21,9 +21,9 @@ void loop(){
   float distance = sonar.ping_cm();
   if (0<distance && distance < DISTANCE_DETECT && bouteilleTrouvee == false ){
     bouteilleTrouvee = true;
-    digitalWrite(cmdRelay, LOW);
+    digitalWrite(CMD_RELAY, LOW);
     delay(TIME_DELAY);
-    digitalWrite(cmdRelay, HIGH);
+    digitalWrite(CMD_RELAY, HIGH);
   }
   else if(bouteilleTrouvee== true && (distance == 0 || distance >DISTANCE_DETECT)){
     bouteilleTrouvee = false;
