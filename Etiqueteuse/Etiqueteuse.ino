62c0637d069378fd97e2550e3e994cdf9ac0bb1c
emmameunier <emma.meunier166@gmail.com>
2022-01-21 11:08:59 +0100

    Ajout d'un vérin stoppeur

--- Etiqueteuse/Etiqueteuse.ino
+++ Etiqueteuse/Etiqueteuse.ino
@@ -1,10 +1,13 @@
 // Programme de détection de bouteille avec actionneur de vérin
+//
+//
 #include <Arduino.h>// Introduction de la bibliothèque Arduino
 #include <NewPing.h>// Introduction de la bibiolthèque NewPin
 
 #define TRIG_PIN 2// Capteur HC_SR04T 
 #define ECHO_PIN 3
-#define CMD_RELAY 10// Relais avec vérin 12V
+#define CMD_RELAY1 11 // Relais vérin Stoppeur
+#define CMD_RELAY2 10 // Relais avec vérin Pousseur
 #define DISTANCE_DETECT 5 // Détection de distance
 #define DISTANCE_MAX 20 // Distance Maximale
 #define TIME_DELAY 2000 // Délais d'attente
@@ -17,17 +20,20 @@ bool timerStarted = false; // Impression état Timer
 
 void setup(){
   Serial.begin(9600);
-  pinMode(CMD_RELAY, OUTPUT); // Action sur le vérin
-  digitalWrite(CMD_RELAY, HIGH); // Démarrage de la boucle vérin rentré
+  pinMode(CMD_RELAY1, OUTPUT); // Sortie vérin Stoppeur 
+  pinMode(CMD_RELAY2, OUTPUT); // Sortie vérin Pousseur
+  digitalWrite(CMD_RELAY1, HIGH); // Démarrage de la boucle vérin stoppeur rentré
+  digitalWrite(CMD_RELAY2, HIGH); // Démarrage de la boucle vérin Pousseur rentré
 }
 void loop(){ // Démarrage de la boucle
   float distance = sonar.ping_cm(); // Distance en décimal, donnée par NewPing
   if (0<distance && distance < DISTANCE_DETECT && !bouteilleTrouvee){ //  Si 0 < distance > DISTANCE_DETECT et pas de bouteille trouvée
     bouteilleTrouvee = true; // Signaler la bouteille trouvée
+    digitalWrite(CMD_RELAY1, LOW); // Action du vérin Stoppeur
     eventDetected = millis(); // EventDetect est égal à la fonction millis
     // Serial.println("trouvee"); 
     timerStarted = true; // Timer commencé
-    digitalWrite(CMD_RELAY, LOW); // Actionnement de vérin 
+    digitalWrite(CMD_RELAY2, LOW); // Actionnement de vérin 
     //delay(TIME_DELAY); // Vérin actionné pendant TIME_DELAY
   }
   else if(bouteilleTrouvee && !timerStarted && (distance == 0 || distance >DISTANCE_DETECT)){ // Si une bouteille est trouvée,et que la distance est égale à zéro ou que la distance est supérieur à DISTANC_DETECT 
@@ -37,9 +43,10 @@ void loop(){ // Démarrage de la boucle
   
   currentTime = millis(); // CurrentTime est égal à millis
   if(timerStarted && (currentTime - eventDetected >= eventInterval)){ // Comparaison temps passé et interval
-    digitalWrite(CMD_RELAY, HIGH); // Rétractation du vérin
+    digitalWrite(CMD_RELAY2, HIGH); // Rétractation du vérin Pousseur
     timerStarted = false; //  Timer terminé
     // Serial.print("timer");
+    digitalWrite(CMD_RELAY1, HIGH); // Rétractation du vérin Stoppeur
   } 
 
 }
